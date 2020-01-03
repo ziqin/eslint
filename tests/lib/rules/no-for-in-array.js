e8e1e2e687b475f148bfd30124ac7bc5e5d163dd
Ziqin Wang <wangzq2017@mail.sustech.edu.cn>
2020-01-03 22:31:22 +0800

    Improve no-for-in-array

--- tests/lib/rules/no-for-in-array.js
+++ tests/lib/rules/no-for-in-array.js
@@ -1,7 +1,8 @@
 /**
- * @fileoverview Try to write my first ESLint rule
+ * @fileoverview Test for no-for-in-array rule.
  * @author Ziqin Wang
  */
+
 "use strict";
 
 //------------------------------------------------------------------------------
@@ -16,22 +17,37 @@ const rule = require("../../../lib/rules/no-for-in-array"),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+    parser: `${__dirname}/../../../lib/flow-parser.js`
+});
 
 ruleTester.run("no-for-in-array", rule, {
-
     valid: [
-        "const arr = [1, 2, 3]; for (const i of arr) console.log(i);",
-        "const arr = [4, 5, 6]; for (const i = 0; i < arr.length; i++) { console.log(arr[i]); } "
+        "var arr = [4, 5, 6]; for (var i = 0; i < arr.length; i++) {} ",
+        {
+            code: "for (let i of [1, 2, 3]) {}",
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "const arr = [1, 2, 3]; for (const i of arr) {}",
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "const obj = {a: 1, b: 2}; for (let attr in obj) {}",
+            parserOptions: { ecmaVersion: 6 }
+        }
     ],
 
     invalid: [
         {
-            code: "const arr = [1, 2, 3]; for (const i in arr) console.log(i);",
-            errors: [{
-                message: "Don't use for-in loop to iterate an array",
-                type: "ForInStatement"
-            }]
+            code: "const arr = [1, 2, 3]; for (const i in arr) {}",
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{ messageId: "noForInArray", type: "ForInStatement" }]
+        },
+        {
+            code: "for (let i in [1, 2, 3]) {}",
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{ messageId: "noForInArray", type: "ForInStatement" }]
         }
     ]
 });
